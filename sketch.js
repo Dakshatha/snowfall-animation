96b6d575534487894e4fa0807149c41efddc9c36
Dakshatha <78903784+Dakshatha@users.noreply.github.com>
2021-04-12 08:47:14 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+let snow = [];
+
+
+
+
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+ 
+
+  
+
+
+
+}
+
+
+function draw() {
+  background(0); 
+  snow.push(new Snowflake);
+
+
+  for(flake of snow) {
+    flake.update();
+flake.render();
+
+  }
+
+}
